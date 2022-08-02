7a4604f4ec278717dc7527864590a5516312e592
jilei-hao <jilei.cs@gmail.com>
2022-08-01 23:10:28 -0400

    issue#17: Code merging and adjustment for frontend

--- OvaryModelGenerator/OvaryModelGenerator.cxx
+++ OvaryModelGenerator/OvaryModelGenerator.cxx
@@ -25,6 +25,10 @@
 #include "vtkNew.h"
 #include "vtkInteractorStyleSwitch.h"
 #include "vtkJSONSceneExporter.h"
+#include "vtkSingleVTPExporter.h"
+#include "vtkProperty.h"
+#include "ColorTable.hxx"
+#include "vtkLookupTable.h"
 
 // Includes for pelvis model
   //First converted from .glb to a multiblock dataset
@@ -34,8 +38,6 @@
   //Then from multiblock data to surface data to polydata
 #include <vtkDataSetSurfaceFilter.h>
 #include <vtkCompositeDataGeometryFilter.h>
-
-#include "TestingHelper.h"
 /**
  * This program generates an ellipsoid (closed surface, vtkPolyData) and converts it into volume
  * representation (vtkImageData) where the foreground voxels are 1 and the background voxels are
@@ -67,12 +69,27 @@ int main(int argc, char* argv[])
   double d = atoi(argv[3]);
   double h = atoi(argv[4]);
   double w = atoi(argv[5]);
+
+  const double scaleRatio = 0.65;
+
+  // Experiment: shrink dims by 10
+  double sd, sh, sw;
+  sd = d * scaleRatio;
+  sh = h * scaleRatio;
+  sw = w * scaleRatio;
+
   const char *outdir = (argc < 7 ? "./output" : argv[6]);
   std::cout<<outdir<<endl;
   if (nslices != 1 && nslices != 3 && nslices != 12)
   {
-      std:cerr<<"Error: nslices must be 1, 3, or 12"<<endl;
-      return EXIT_FAILURE;
+    std:cerr<<"Error: nslices must be 1, 3, or 12"<<endl;
+    return EXIT_FAILURE;
+  }
+
+  if (nrot != 1 && nrot != 2 && nrot != 4)
+  {
+    std::cerr<<"Error: Can only input 1, 2, or 4 in the nrot field"<<endl;
+    return EXIT_FAILURE;
   }
   // if outdir does not include a '/', add it
   std::string outdirstr = outdir;
@@ -86,30 +103,24 @@ int main(int argc, char* argv[])
   std::string fnxmlpel = outdirstr + filename_p + ".vtp";
   std::string fnjson = outdirstr + filename;
 
-  //std::cout << "fnimg=" << fnimg << std::endl;
-  //std::cout << "fnmesh=" << fnmesh << std::endl;
-
   // reading in pelvis data
 
-    //reads in data from the .glb file
-  vtkSmartPointer<vtkGLTFReader> pelvisReader =
-    vtkSmartPointer<vtkGLTFReader>::New();
-  pelvisReader->SetFileName("D:/VSCodeProjects/PelvisModel/VH_F_Pelvis.glb");
+  //reads in data from the .glb file
+  vtkNew<vtkGLTFReader> pelvisReader;
+  pelvisReader->SetFileName("/Users/jileihao/data/data_hubmap/pelvis_model/VH_F_Pelvis.glb");
   pelvisReader->Update();
 
-    //creates multiblock dataset to extract surface data from to convert into polydata
+  //creates multiblock dataset to extract surface data from to convert into polydata
   vtkSmartPointer<vtkMultiBlockDataSet> pelvis_mb = pelvisReader->GetOutput(); 
-
 
   // specifying spheroid scale
   double Rsphere = 30;
-  double sx = 0.5*d/Rsphere;
-  double sy = 0.5*h/Rsphere;
-  double sz = 0.5*w/Rsphere;
+  double sx = 0.5*sd/Rsphere;
+  double sy = 0.5*sh/Rsphere;
+  double sz = 0.5*sw/Rsphere;
 
   // create a spheroid with specified dimensions
-  vtkSmartPointer<vtkSphereSource> sphereSource = 
-    vtkSmartPointer<vtkSphereSource>::New();  
+  vtkNew<vtkSphereSource> sphereSource;
   sphereSource->SetThetaResolution(100);
   sphereSource->SetPhiResolution(100);
   sphereSource->SetRadius(Rsphere);
@@ -117,30 +128,25 @@ int main(int argc, char* argv[])
   sphereSource->Update();
 
   //convert pelvis model to polydata
-
-    //produces surface data from multiblock dataset
+  //produces surface data from multiblock dataset
   vtkNew<vtkDataSetSurfaceFilter> pelvis_surfaces; 
   pelvis_surfaces->SetInputData(pelvis_mb);
 
-    //compiles surface data into a single polydata object
+  //compiles surface data into a single polydata object
   vtkNew<vtkCompositeDataGeometryFilter> pelvis_comp;
   pelvis_comp->SetInputConnection(pelvis_surfaces->GetOutputPort());
   pelvis_comp->Update();
 
   //rescaling parameters for ovary
-  vtkSmartPointer<vtkTransform> rescale = 
-    vtkSmartPointer<vtkTransform>::New();
+  vtkNew<vtkTransform> rescale;
   rescale->Scale(sx, sy, sz);
 
-
   //rescale ovary polydata
-  vtkSmartPointer<vtkTransformPolyDataFilter> rescaleFilter = 
-    vtkSmartPointer<vtkTransformPolyDataFilter>::New();
+  vtkNew<vtkTransformPolyDataFilter> rescaleFilter;
   rescaleFilter->SetInputConnection(sphereSource->GetOutputPort());
   rescaleFilter->SetTransform(rescale);
   rescaleFilter->Update();
   vtkSmartPointer<vtkPolyData> pd_trans = rescaleFilter->GetOutput();
-
 
   //rescale pelvis polydata
   vtkSmartPointer<vtkPolyData> pelvis_pd = pelvis_comp->GetOutput();
@@ -148,57 +154,27 @@ int main(int argc, char* argv[])
   double pel_bounds[6];
   pelvis_pd->GetBounds(pel_bounds);
 
-    //Uncomment for original pelvis model bounds (x, y, z)
-  /*std::cout << "xmin: " << pel_bounds[0] << " "
-            << "xmax: " << pel_bounds[1] << std::endl
-            << "ymin: " << pel_bounds[2] << " "
-            << "ymax: " << pel_bounds[3] << std::endl
-            << "zmin: " << pel_bounds[4] << " "
-            << "zmax: " << pel_bounds[5] << std::endl;*/
-
   double x_width;
-  //double y_height;
-  //double z_depth;
 
   double ovary_bounds[6];
   pd_trans->GetBounds(ovary_bounds);
-
   double z_ovaryDepth = ovary_bounds[5] - ovary_bounds[4];
-
   x_width = pel_bounds[1] - pel_bounds[0];
-  //y_height = pel_bounds[3] - pel_bounds[2];
-  //z_depth = pel_bounds[5] - pel_bounds[4];
-
-    //Uncomment for original pelvis model dimensions
-  /*std::cout << "Width:  " << x_width << std::endl
-            << "Height: " << y_height << std::endl
-            << "Depth:  " << z_depth << std::endl;*/
-
 
   //rescaling parameters for pelvis
-  double scaleWidth = 290 / x_width; //average intercristal distance / original model width = scale factor to get a 29 cm (avg width) pelvis
-  std::cout << "Scale factor: " << scaleWidth << std::endl;
-  double scaleHeight = scaleWidth;
-  double scaleDepth = scaleWidth;
-
-  vtkSmartPointer<vtkTransform> pel_rescale = 
-    vtkSmartPointer<vtkTransform>::New();
-  pel_rescale->Scale(scaleWidth, scaleHeight, scaleDepth);
-
+  double pelScale = 290 / x_width; //average intercristal distance / original model width = scale factor to get a 29 cm (avg width) pelvis
+  vtkNew<vtkTransform> pel_rescale;
+  pel_rescale->Scale(pelScale, pelScale, pelScale);
 
   //rescale pelvis polydata
-  vtkSmartPointer<vtkTransformPolyDataFilter> rescalePelvisFilter = 
-    vtkSmartPointer<vtkTransformPolyDataFilter>::New();
+  vtkNew<vtkTransformPolyDataFilter> rescalePelvisFilter;
   rescalePelvisFilter->SetInputConnection(pelvis_comp->GetOutputPort());
   rescalePelvisFilter->SetTransform(pel_rescale);
   rescalePelvisFilter->Update();
-
   vtkSmartPointer<vtkPolyData> pelvisTransform_pd = rescalePelvisFilter->GetOutput();
 
-
   // create an image of the spheroid
-  vtkSmartPointer<vtkImageData> whiteImage = 
-    vtkSmartPointer<vtkImageData>::New();    
+  vtkNew<vtkImageData> whiteImage;  
   double bounds[6];
   pd_trans->GetBounds(bounds);
   double spacing[3]; // desired volume spacing
@@ -211,10 +187,7 @@ int main(int argc, char* argv[])
   int dim[3];
   for (int i = 0; i < 3; i++)
     {
-    //dim[i] = static_cast<int>(ceil((bounds[i * 2 + 1] - bounds[i * 2]) / spacing[i])+1);
     dim[i] = static_cast<int>(ceil((bounds[5] - bounds[4])/spacing[2])+1); 
-    //std::cerr << "dim " << dim[i] << std::endl;
-    //std::cerr << "bounds[] " << bounds[3] << " " << bounds[4] << " " << bounds[5] << endl;
     }
   whiteImage->SetDimensions(dim);
   whiteImage->SetExtent(-1, dim[0] - 1, -1, dim[1] - 1, -1, dim[2] - 1);
@@ -237,8 +210,7 @@ int main(int argc, char* argv[])
     }
 
   // polygonal data --> image stencil:
-  vtkSmartPointer<vtkPolyDataToImageStencil> pol2stenc = 
-    vtkSmartPointer<vtkPolyDataToImageStencil>::New();
+  vtkNew<vtkPolyDataToImageStencil> pol2stenc;
 
   pol2stenc->SetInputData(pd_trans);
   pol2stenc->SetOutputOrigin(origin);
@@ -322,16 +294,10 @@ int main(int argc, char* argv[])
               pix[0] = pix[0] + pix[0]*(slice_num-1+3*nslices);
             }
           }
-          else
-          {
-            std::cerr<<"Error: Can only input 1, 2, or 4 in the nrot field"<<endl;
-            return EXIT_FAILURE;
-          }
+
         }
       }
     }
-
-
 
 /*
   // write the label image for troubleshooting
@@ -346,9 +312,6 @@ int main(int argc, char* argv[])
   // Run marching cubes on the image to convert it back to VTK polydata
   vtkPolyData *pipe_tail;
 
-  // Append filter for assembling labels
-  vtkAppendPolyData *fltAppend = vtkAppendPolyData::New();
-
   // Extracting one label at a time and assigning label value
   float imax;
   if (nrot == 0)
@@ -360,13 +323,12 @@ int main(int argc, char* argv[])
   std::vector<vtkSmartPointer<vtkPolyData>> vec;
   for (float i = 1; i <= imax; i += 1.0)
     {
-    
     float lbl = floor(i);
 
     std::cout << "  -- Processing Label: " << lbl << std::endl;
 
     // Extract one label
-    vtkDiscreteMarchingCubes *fltDMC = vtkDiscreteMarchingCubes::New();
+    vtkNew<vtkDiscreteMarchingCubes> fltDMC;
     fltDMC->SetInputData(mlImage);
     fltDMC->ComputeGradientsOff();
     fltDMC->ComputeScalarsOff();
@@ -377,36 +339,46 @@ int main(int argc, char* argv[])
 
     vtkSmartPointer<vtkPolyData> labelMesh = fltDMC->GetOutput();
 
+
     // Set scalar values for the label
-    vtkUnsignedShortArray *scalar = vtkUnsignedShortArray::New();
+    vtkNew<vtkUnsignedShortArray> scalar;
     scalar->SetNumberOfComponents(1);
-    for (vtkIdType i = 0; i < labelMesh->GetNumberOfPoints(); ++i)
-    {
-      scalar->InsertNextTuple1(lbl);
-    }
-
+    scalar->InsertNextTuple1(lbl);
     scalar->SetName("Label");
-    labelMesh->GetPointData()->SetScalars(scalar);
-    fltAppend->AddInputData(labelMesh);
+    labelMesh->GetFieldData()->AddArray(scalar);
+    
+    // Create the transform filter for ovary
+    vtkTransformPolyDataFilter *fltTransform = vtkTransformPolyDataFilter::New();
+    fltTransform->SetInputData(labelMesh);
+
+    vtkSmartPointer<vtkTransform> vox2coords =
+      vtkSmartPointer<vtkTransform>::New();
+    vox2coords->Translate(0,0,0);
+
+    // Experiment: Scale size back
+    vtkNew<vtkTransform> scaleBack;
+    double sbr = 1/scaleRatio; // scale back ratio
+    scaleBack->Scale(sbr, sbr, sbr);
+    vox2coords->Concatenate(scaleBack);
+
+    // Update the VTK transform to match
+    fltTransform->SetTransform(vox2coords);
+    fltTransform->Update();
+    labelMesh = fltTransform->GetOutput();
+
+    // smoothing  
+    
+    vtkNew<vtkWindowedSincPolyDataFilter> smoothFilter;
+    smoothFilter->SetInputData(labelMesh);
+    smoothFilter->SetNumberOfIterations(50);
+    smoothFilter->SetPassBand(0.05);
+    smoothFilter->SetNonManifoldSmoothing(false);
+    smoothFilter->Update();
+    labelMesh = smoothFilter->GetOutput();
+    
+    // Get final output
+    vec.push_back(labelMesh);
     }
-
-  fltAppend->Update();
-  pipe_tail = fltAppend->GetOutput();
-
-  // Create the transform filter for ovary
-  vtkTransformPolyDataFilter *fltTransform = vtkTransformPolyDataFilter::New();
-  fltTransform->SetInputData(pipe_tail);
-
-  vtkSmartPointer<vtkTransform> vox2coords =
-    vtkSmartPointer<vtkTransform>::New();
-  vox2coords->Translate(0.2,0.2,0.2);
-
-  // Update the VTK transform to match
-  fltTransform->SetTransform(vox2coords);
-  fltTransform->Update();
-
-  // Get final output
-  vtkPolyData *mesh = fltTransform->GetOutput();
 
   //Transforming pelvis model
 
@@ -417,110 +389,102 @@ int main(int argc, char* argv[])
 
   vtkTransform *pelTransform = vtkTransform::New();
 
-    //Rotation
+  //Rotation
   pelTransform->RotateX(45);
   pelTransform->RotateY(-90);
   pelTransform->RotateZ(-10);
   
-    //Translation
+  //Translation
   pelTransform->Translate(-30, -35, 80);
 
-    //Concatenate - allows for multiple transformation steps to be applied to a preset matrix
+  //Concatenate - allows for multiple transformation steps to be applied to a preset matrix
   pelTransform->Concatenate(baseMatrix);
 
-    //Applying transformation to pelvis polydata
+  //Applying transformation to pelvis polydata
   vtkTransformPolyDataFilter *pelTransformFilter = vtkTransformPolyDataFilter::New();
   pelTransformFilter->SetTransform(pelTransform);
   pelTransformFilter->SetInputData(pelvisTransform_pd);
   pelTransformFilter->Update();
 
-    //Translation
-  /*vtkSmartPointer<vtkTransform> pel_translation =
-    vtkSmartPointer<vtkTransform>::New();
-  pel_translation->Translate(0, -40.0, 20);
-
-  pelTransform_1->SetTransform(pel_translation);
-  pelTransform_1->Update();
-
-  pelvisTransform_pd = pelTransform_1->GetOutput();
-
-    //New polydata with translation
-  vtkTransformPolyDataFilter *pelTransform_2 = vtkTransformPolyDataFilter::New();
-  pelTransform_2->SetInputData(pelvisTransform_pd);
-
-    //Rotation
-
-  vtkSmartPointer<vtkTransform> pel_rotation =
-    vtkSmartPointer<vtkTransform>::New();
-  pel_rotation->RotateY(-90);
-  //pel_rotation->RotateZ(120);
-
-    //Update transform
-  pelTransform_2->SetTransform(pel_rotation);
-  pelTransform_2->Update();*/
-
-    //Retrieving output polydata of filter application
+  //Retrieving output polydata of filter application
   vtkSmartPointer<vtkPolyData> pelFinal_pd = pelTransformFilter->GetOutput();
 
-  // write the label mesh
-  /*
-  vtkSmartPointer<vtkPolyDataWriter> meshWriter = 
-    vtkSmartPointer<vtkPolyDataWriter>::New();
-  
-  meshWriter->SetFileName(fnmesh.c_str());
-  meshWriter->SetInputData(mesh);
-  meshWriter->Write();
-  */
-
-  // smoothing
-  vtkNew<vtkWindowedSincPolyDataFilter> smoothFilter;
-  smoothFilter->SetInputData(mesh);
-  smoothFilter->SetNumberOfIterations(20);
-  smoothFilter->SetPassBand(0.001);
-  smoothFilter->SetNonManifoldSmoothing(false);
-  smoothFilter->Update();
-  
-  vec.push_back(smoothFilter->GetOutput());
-
-  //write pelvis vtp
-  vtkNew<vtkXMLPolyDataWriter> xmlPelWriter;
-  xmlPelWriter->SetFileName(fnxmlpel.c_str());
-  xmlPelWriter->SetInputData(pelFinal_pd);
-  xmlPelWriter->Write();
-  
   // write vtp mesh
-
   vtkNew<vtkRenderer> ren;
   vtkNew<vtkRenderWindow> renWin;
   vtkNew<vtkRenderWindowInteractor> iren;
 
-    
+  ColorTable *ct = new ColorTable();
+  vtkNew<vtkLookupTable> lut;
+  lut->SetRange(1, imax);
+  lut->SetSaturationRange(0.7, 1);
+  lut->SetValueRange(0.7, 1);
+  lut->Build();
   for(auto mesh : vec)
   {
       vtkNew<vtkActor> actor;
       vtkNew<vtkPolyDataMapper> mapper;
       mapper->SetInputData(mesh);
+
+      auto fd = mesh->GetFieldData();
+      auto label = fd->GetArray("Label")->GetTuple1(0);
+      
+      uint8_t r, g, b;
+      float a;
+      ct->GetLabelColor(label, r, g, b, a);
+
+      double c[3];
+
+      /*
+      c[0] = r/255.0;
+      c[1] = g/255.0;
+      c[2] = b/255.0;
+      */
+      lut->GetColor(label, c);
+      actor->GetProperty()->SetColor(c);
       actor->SetMapper(mapper);
       ren->AddActor(actor);
   }
-    
-  renWin->AddRenderer(ren);
 
+  vtkNew<vtkActor> pvsActor;
+  vtkNew<vtkPolyDataMapper> pvsMapper;
+  pvsMapper->SetInputData(pelFinal_pd);
+  pvsActor->SetMapper(pvsMapper);
+  pvsActor->GetProperty()->SetOpacity(0.05);
+  ren->AddActor(pvsActor);
+
+
+  renWin->AddRenderer(ren);
   iren->SetRenderWindow(renWin);
+  ren->SetBackground(0.32, 0.34, 0.43);
+  ren->ResetCamera();
+  iren->Initialize();
+  
+  
+  // Display structure for troubleshooting
+  /*
   vtkNew<vtkInteractorStyleSwitch> iSwitch;
   iSwitch->SetCurrentStyleToTrackballCamera();
   iren->SetInteractorStyle(iSwitch);
-
-  renWin->SetSize(800, 600);
-  ren->SetBackground(0.4, 0.5, 0.6);
-  ren->ResetCamera();
-  // ren->Render();
-  iren->Initialize();
+  ren->Render();
+  iren->Start();
+  */
 
   vtkNew<vtkJSONSceneExporter> exp;
   exp->SetRenderWindow(renWin);
   exp->SetActiveRenderer(ren);
   exp->SetFileName(fnjson.c_str());
   exp->Write();
+
+  // Export a vtp scene for troubleshooting
+  /*
+  vtkNew<vtkSingleVTPExporter> vtpExp;
+  vtpExp->SetRenderWindow(renWin);
+  vtpExp->SetActiveRenderer(ren);
+  std::string fnvtp = fnjson + ".vtp";
+  vtpExp->SetFileName(fnvtp.c_str());
+  vtpExp->Write();
+  */
+
   return EXIT_SUCCESS;
 }
